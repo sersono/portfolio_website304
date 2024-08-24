fe27fac1e93d2292045387baaf04c860365155a2
sersono <80944632+sersono@users.noreply.github.com>
2024-08-25 07:54:08 +0900

    JS-ArrowUp

--- src/main.js
+++ src/main.js
@@ -1,39 +1,3 @@
-'use strict';
-
-// document.querySelector('.header');
-
-// window.document.querySelector();
-// 브라우저가 제공하는 객체, 글로벌객체
-
-// const header = document.querySelector('.header');
-// console.log(header);
-
-// const header = document.querySelector('.header');
-// const headerRect = header.getBoundingClientRect();
-// console.log(headerRect);
-// const headerHeight = headerRect.height;
-
-// const header = document.querySelector('.header');
-// const headerHeight = header.getBoundingClientRect().heiight;
-// document.addEventListener('scroll', function () {
-    // 이름 없는 함수
-    // 하고 싶은 일 작성
-    // document.addEventListener('scroll', () => {
-// })
-
-// const header = document.querySelector('.header');
-// const headerHeight = header.getBoundingClientRect().heiight;
-// document.addEventListener('scroll', () => {
-    // 스크롤되는 Y좌표가 headerHeight보다 크면 다른 스타일링!
-    // console.log(window.scrollY);
-    // if (window.scrollY > headerHeight) {
-        // header.classList.add('header--dark');
-    // } else {
-        // header.classList.remove('header--dark');
-    // }
-// });
-
-
 // Header에 페이지 아래로 스크롤시 다크 스타일링 적용
 const header = document.querySelector('.header');
 const headerHeight = header.getBoundingClientRect().height;
@@ -45,3 +9,20 @@ document.addEventListener('scroll', () => {
     }
 });
 
+
+// Prologue 섹션을 아래로 스크롤시 투명하게 처리함
+const prologue = document.querySelector('.prologue__container');
+const prologueHeight = prologue.offsetHeight;
+document.addEventListener('scroll', () => {
+    prologue.style.opacity = 1 - window.scrollY / prologueHeight;
+});
+
+// Arrow up버튼을 아래로 스크롤시 투명하게 처리함
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+    if (window.scrollY > prologueHeight / 2) {
+        arrowUp.style.opacity = 1;
+    } else {
+        arrowUp.style.opacity = 0;
+    }
+});
